80628fa41319d5bf42a39a2f99f144f0dd24e8f9
Overpower999 <ageev20111@gmail.com>
2023-04-05 11:31:09 +0300

    homework Part 2

---
+++ src/part2/Car.java
@@ -0,0 +1,18 @@
+package part2;
+
+public class Car {
+    private void startElectricity(){
+        System.out.println("asdasd");
+    }
+    private void startCommand(){
+        System.out.println("asdasdasdxcx122");
+    }
+    private void startFuelSystem(){
+        System.out.println("sdasdqw12341     qweq");
+    }
+    public void start(){
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+}
---
+++ src/part2/Main.java
@@ -0,0 +1,8 @@
+package part2;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.start();
+    }
+}
